a57444593c6d30f52aeaae4dd68dad9cce9fb976
tdknE <thalesendo@gmail.com>
2025-11-09 00:43:14 -0300

    Delete Test directory

--- Test/nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const gridWrap = document.querySelector(".grid-wrap");
-    const navButton = document.getElementById("nav-mobile-button");
-    navButton.addEventListener('click',function(){
-        event.preventDefault();
-        gridWrap.classList.toggle('menu-open');
-        navButton.classList.toggle('fa-solid fa-bars')
-    })
-})
